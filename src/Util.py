68931e9470496d789bae0e172c06d5a7dcdbb466
Alex Chavez <alex@alexchavez.net>
2016-08-02 06:22:20 +0000

    Got some lyrics and created a new notebook to explore text clustering

--- src/Util.py
+++ src/Util.py
@@ -2,6 +2,9 @@ import os
 import datetime
 import pandas as pd
 import sqlite3
+import urllib
+from urlparse import urlparse
+import lxml.html
 
 def execute_query(db_path, db_name, query):
     ### Returns a DataFrame containing the query results
@@ -17,3 +20,115 @@ def execute_query(db_path, db_name, query):
 # a time lag in seconds
 def strtimedelta(starttime, stoptime):
     return datetime.timedelta(seconds=stoptime-starttime)
+
+# Sebastian Raschka, 2014
+# 
+# Script to download lyrics from http://lyrics.wikia.com/
+
+class Song(object):
+    def __init__(self, artist, title):
+        self.artist = self.__format_str(artist)
+        self.title = self.__format_str(title)
+        self.url = None
+        self.lyric = None
+        
+    def __format_str(self, s):
+        # remove paranthesis and contents
+        s = s.strip()
+        try:
+            # strip accent
+            s = ''.join(c for c in unicodedata.normalize('NFD', s)
+                         if unicodedata.category(c) != 'Mn')
+        except:
+            pass
+        s = s.title()
+        return s
+        
+    def __quote(self, s):
+#          return urlparse.quote(s.replace(' ', '_'))
+        return s.replace(' ', '_')
+
+    def __make_url(self):
+        artist = self.__quote(self.artist)
+        title = self.__quote(self.title)
+        artist_title = '%s:%s' %(artist, title)
+        url = 'http://lyrics.wikia.com/' + artist_title
+        self.url = url
+        
+    def update(self, artist=None, title=None):
+        if artist:
+            self.artist = self.__format_str(artist)
+        if title:
+            self.title = self.__format_str(title)
+        
+    def lyricwikia(self):
+        self.__make_url()
+        try:
+            doc = lxml.html.parse(self.url)
+            lyricbox = doc.getroot().cssselect('.lyricbox')[0]
+        except (IOError, IndexError) as e:
+            self.lyric = ''
+            return self.lyric
+        lyrics = []
+
+        for node in lyricbox:
+            if node.tag == 'br':
+                lyrics.append('\n')
+            if node.tail is not None:
+                lyrics.append(node.tail)
+        self.lyric =  "".join(lyrics).strip()    
+        return self.lyric
+
+class Song(object):
+    def __init__(self, artist, title):
+        self.artist = self.__format_str(artist)
+        self.title = self.__format_str(title)
+        self.url = None
+        self.lyric = None
+        
+    def __format_str(self, s):
+        # remove paranthesis and contents
+        s = s.strip()
+        try:
+            # strip accent
+            s = ''.join(c for c in unicodedata.normalize('NFD', s)
+                         if unicodedata.category(c) != 'Mn')
+        except:
+            pass
+        s = s.title()
+        return s
+        
+    def __quote(self, s):
+#          return urlparse.quote(s.replace(' ', '_'))
+        return s.replace(' ', '_')
+
+    def __make_url(self):
+        artist = self.__quote(self.artist)
+        title = self.__quote(self.title)
+        artist_title = '%s:%s' %(artist, title)
+        url = 'http://lyrics.wikia.com/' + artist_title
+        self.url = url
+        
+    def update(self, artist=None, title=None):
+        if artist:
+            self.artist = self.__format_str(artist)
+        if title:
+            self.title = self.__format_str(title)
+        
+    def lyricwikia(self):
+        self.__make_url()
+        try:
+            doc = lxml.html.parse(self.url)
+            lyricbox = doc.getroot().cssselect('.lyricbox')[0]
+        except (IOError, IndexError) as e:
+            self.lyric = ''
+            return self.lyric
+        lyrics = []
+
+        for node in lyricbox:
+            if node.tag == 'br':
+                lyrics.append('\n')
+            if node.tail is not None:
+                lyrics.append(node.tail)
+        self.lyric =  "".join(lyrics).strip()    
+        return self.lyric
